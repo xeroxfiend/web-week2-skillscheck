80df3631cce99b3946ca05700b1224d8b8135324
Brian Lowe <mynameisboy@gmail.com>
2019-08-22 11:06:38 -0600

    finished

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -4,27 +4,31 @@
 //////////////////STEP 1////////////////////
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
 
-// Code Here
+const lovesCode = true
 
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
 
-// Code Here
+const age = 31
 
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
 
-// Code Here
+const name = 'Brian'
 
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
 
-// Code Here
+let phrase = ''
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
-// Code Here
+if (lovesCode) {
+    phrase = 'I love code'
+} else {
+    phrase = 'I do not love to code'
+}
 
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
